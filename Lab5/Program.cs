85777e076b9086c9c7131351e41f5e6e64f9eaf4
Rassokhin Anton <antonrassohin@gmail.com>
2021-11-19 23:06:15 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 7; //или заменить на требуемое кол-во ячеек в массиве
+            int[] array = new int [n];
+            double s = 0;
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+            }
+            double x = s / n;
+            Console.WriteLine("Среднее арифметическое значение элементов массива ={0:f2}", x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во строк=столбцов массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n,n];
+            int x = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    x++;
+                    if ((x % 2) != 0)
+                    {
+                        array[i, j] = 1;
+                        Console.Write("{0} ",array[i,j]);
+                    }
+                    else
+                    {
+                        array[i, j] =0;
+                        Console.Write("{0} ", array[i, j]);
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
